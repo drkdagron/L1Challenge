a8ed3351de9c31ab6252f3e44183f582a27059cc
Stephen McKillop <Stephen McKillop>
2016-07-11 12:42:05 -0400

    Challenge Two

--- routes/main.js
+++ routes/main.js
@@ -1,10 +1,4 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-app.use(bodyParser.urlencoded({extended: false }));
-app.use(bodyParser.json());
-app.use('view-engine', 'ejs');
-
+var route = function(app) {
 app.get('/', function(req, res) {
     res.status(200).send("Hello World");
 });
@@ -13,12 +7,13 @@ app.get('/api/:response', function(req, res){
     res.status(201).send(req.params.response);
 });
 
+app.post('/', jsonParser, function(req, res) {
+    
+});
+
 app.param('response', function(req, res, next, id) {
     //console.log(req.params.response);
     next();
 });
-
-var server = app.listen(3000, function(e) {
-});
-
-module.exports = server;
+};
+module.exports = route;
--- test/tests.js
+++ test/tests.js
@@ -1,14 +1,18 @@
 var request = require('supertest');
+var expect = require('chai').expect;
+
+var server;
+describe('Loyalty One Tests', function() {
+
+before(function () {
+    server = require('../server');
+});
+
+after (function () {
+    server.close();
+});
 
 describe('Server starting', function() {
-    var server;
-    before(function() {
-        server = require('../server');
-    });
-    after(function() {
-        server.close();
-    });
-
     it ('Successful start of site', function foundRoot(done) {
         request(server)
             .get('/')
@@ -19,4 +23,24 @@ describe('Server starting', function() {
             .get('/nothing/please')
             .expect (404, done);
     });
+});
+
+describe('REST return text', function () {
+    it ('Recieve 201 status if calling rest', function seperateStatus(done) {
+        request(server)
+            .get('/api/restresponse')
+            .expect(201, done);
+    });
+    it ('Recieve REST info for display', function grabRestInfo(done) {
+        request(server)
+            .get('/api/restresponse')
+            .expect(201)
+            .end(function(err, res){
+                expect(res.text).to.equal('restresponse');
+                done();
+            });
+    });
+});
+
+
 });
